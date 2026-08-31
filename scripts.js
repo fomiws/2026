39b20a5ab087b7a6528a25fa6989248bbef13633
wterkaj <wterkaj@gmail.com>
2026-08-31 17:51:19 +0200

    pc

--- scripts.js
+++ scripts.js
@@ -33,33 +33,27 @@ const organizers = [
 ]
 const committeeList = ''
 
-// '<li>Alessandro Umbrico, CNR-ISTC, Italy</li>' +
-// '<li>Alex Donkers, Eindhoven University of Technology, Netherlands</li>' +
-// '<li>Antonio Maria Rinaldi, University of Naples Federico II, Italy</li>' +
-// '<li>Bahar Aameri, University of Toronto, Canada</li>' +
-// '<li>Boonserm Kulvatunyou, National Institute of Standards and Technology, USA</li>' +
-// '<li>Cornelis Bouter, TNO, Netherlands</li>' +
-// '<li>Daniele Spoladore, CNR-STIIMA, Italy</li>' +
-// '<li>Emilio Sanfilippo, CNR-ISTC, Italy</li>' +
-// '<li>Francesco Zaccarini, University of Bologna, Italy</li>' +
-// '<li>Glenda Amaral, Central Bank of Brazil, Brazil</li>' +
-// '<li>Hervé Panetto, University of Lorraine, France</li>' +
-// '<li>Jean Paul Sebastian Piest, University of Twente, Netherlands</li>' +
-// '<li>João Paulo Almeida, UFES, Brazil</li>' +
-// '<li>Kelli Cordeiro, Ministry of Defense, Brazil</li>' +
-// '<li>Laura Daniele, TNO, Netherlands</li>' +
-// '<li>Laure Vieu, IRIT-CNRS, France</li>' +
-// '<li>Luís Ferreira Pires, University of Twente, Netherlands</li>' +
-// '<li>Luiz Olavo Bonino Da Silva Santos, University of Twente, Netherlands</li>' +
-// '<li>Marcela Vegetti, CONICET, Argentina</li>' +
-// '<li>María Poveda, Universidad Politécnica de Madrid, Spain</li>' +
-// '<li>Patricio de Alencar Silva, UFERSA, Brazil</li>' +
-// '<li>Pedro Paulo F. Barcelos, University of Twente, Netherlands</li>' +
-// '<li>Peter Klein, Fraunhofer ITWM, Germany</li>' +
-// '<li>Pieter Pauwels, Eindhoven University of Technology, Netherlands</li>' +
-// '<li>Rebeca Arista, Airbus, France</li>' +
-// '<li>Riichiro Mizoguchi, Japan Advanced Institute of Science and Technology, Japan</li>' +
-// '<li>Silvia Chiacchiera, STFC UKRI, UK</li>'
+'<li>Alessandro Oltramari, Bosch, USA</li>' +
+'<li>Alessandro Umbrico, CNR-STIIMA, Italy</li>' +
+'<li>Alex Donkers, Eindhoven University of Technology, Netherlands</li>' +
+'<li>Bahar Aameri, University of Toronto, Canada</li>' +
+'<li>Claudenir Fonseca, University of Twente, Netherlands</li>' +
+'<li>Cornelis Bouter, TNO, Netherlands</li>' +
+'<li>Daniele Spoladore, CNR-STIIMA, Italy</li>' +
+'<li>Elisa Negri, Politecnico di Milano, Italy</li>' +
+'<li>Fabio Longo, CNR-STIIMA, Italy</li>' +
+'<li>Francesco Zaccarini, University of Bologna, Italy</li>' +
+'<li>Kelli Cordeiro, Ministry of Defense, Brazil</li>' +
+'<li>Laure Vieu, IRIT-CNRS, France</li>' +
+'<li>María Poveda, Universidad Politécnica de Madrid, Spain</li>' +
+'<li>Pieter Pauwels, Eindhoven University of Technology, Netherlands</li>' +
+'<li>Rodrigo Calhau, IFES, Brazil</li>' +
+'<li>Rebeca Arista, Airbus, France</li>' +
+'<li>Riichiro Mizoguchi, Japan Advanced Institute of Science and Technology, Japan</li>' +
+'<li>Silvia Chiacchiera, STFC UKRI, UK</li>' +
+'<li>Stefano Borgo, CNR-STIIMA, Italy</li>' +
+'<li>Peter Klein, Fraunhofer ITWM, Germany</li>'
+
 
 function createTable() {
     var tableHTML = '';
